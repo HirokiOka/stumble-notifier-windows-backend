90689d8137bde45c2b05026072828dd4996d8632
hiroka <mementoka@gmail.com>
2022-12-10 14:41:50 +0900

    fix csv read

---
+++ endpoint.py
@@ -0,0 +1,52 @@
+import pickle
+from read_heart_data import get_latest_heart_rate_data, calc_pnn50
+
+
+stumble_seq_length = 60
+multi_model_bin_path = './models/multi_model.pickle'
+code_model_bin_path = './models/code_model.pickle'
+
+with open(multi_model_bin_path, 'rb') as f:
+    multi_model = pickle.load(f)
+with open(code_model_bin_path, 'rb') as f:
+    code_model = pickle.load(f)
+
+
+"""
+ - get row data from csv and DB
+ - caclulate features
+    - pNN50 (Done)
+    - elapsed seconds
+ - post-processing
+"""
+
+
+# SLOC, AST ED, elapsed-sec, lf/hf, pnn50
+def classify_stumble(feature_data, mode='multi'):
+    classified_result = []
+    if (mode == 'code'):
+        classified_result = code_model.predict(feature_data)
+    elif (mode == 'multi'):
+        classified_result = multi_model.predict(feature_data)
+    return classified_result
+
+
+def post_process_stumbles(state_queue, ratio=2/3):
+    if (len(state_queue) < stumble_seq_length):
+        return None
+    result = 0
+    threshold = int(len(state_queue) * ratio)
+    if (state_queue.count(0) > threshold):
+        result = 1
+    else:
+        result = 0
+    return result
+
+
+
+
+
+sample_code_features = [[24, 2, 200], [24, 2, 200]]
+sample_multi_features = [[7.4, 10.0, 24, 2, 200], [2.0, 12.0, 24, 2, 200]]
+print(classify_stumble(sample_code_features, mode='code'))
+print(classify_stumble(sample_multi_features, mode='multi'))
---
+++ read_heart_data.py
@@ -0,0 +1,42 @@
+import csv
+
+PNN50_WINDOW_SIZE = 100
+LOWER_RRI_THRESHOLD = 200
+UPPER_RRI_THRESHOLD = 1100
+
+
+def calc_pnn50(rri_chunk):
+    if (len(rri_chunk) < PNN50_WINDOW_SIZE):
+        return 0
+    last_rri = 0
+    all_rri_diff = []
+    for v in rri_chunk:
+        current_rri = last_rri if ((int(v) < LOWER_RRI_THRESHOLD) or (UPPER_RRI_THRESHOLD < int(v))) else int(v)
+        rri_diff = abs(current_rri - last_rri)
+        all_rri_diff.append(rri_diff)
+        last_rri = current_rri
+    all_rri_diff.pop(0)
+    all_nn50 = [v for v in all_rri_diff if v > 50]
+    pnn50 = len(all_nn50) / PNN50_WINDOW_SIZE
+    return pnn50
+
+
+# return a list of [time, RRI, lf/hf]
+def get_latest_heart_rate_data(file_path):
+    result = []
+    with open(file_path, encoding="cp932", mode="r") as f:
+        data = list(csv.reader(f))[6:]
+        number_of_data = 0
+        if (len(data) > PNN50_WINDOW_SIZE):
+            number_of_data = 100
+        else:
+            number_of_data = len(data)
+        heart_rate_data = data[:number_of_data]
+        result = [[d[0], d[1], d[7]] for d in heart_rate_data]
+    return result
+
+
+# Test
+test_path = './whs-data/test_1.csv'
+# get_latest_heart_rate_data(test_path)
+print(get_latest_heart_rate_data(test_path))
