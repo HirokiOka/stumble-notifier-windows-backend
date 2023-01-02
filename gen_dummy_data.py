720eb9c11a644239004bc9d8ccd7aef6aa17b4e3
hiroka <mementoka@gmail.com>
2023-01-02 13:41:59 +0900

    add append_dummy_row_to_csv function

--- gen_dummy_data.py
+++ gen_dummy_data.py
@@ -1,4 +1,5 @@
 import random
+import csv
 import time
 
 
@@ -16,7 +17,21 @@ def gen_dummy_features():
     return [[sloc, ted, elapsed_sec, lf_hf, pnn50]]
 
 
+def append_dummy_row_to_csv(csv_path):
+    dummy_data = gen_dummy_features()
+    with open(csv_path, 'a', encoding="cp932") as f:
+        writer = csv.writer(f)
+        writer.writerow(dummy_data[0])
+        print(f'{dummy_data[0]} appended to {csv_path}')
+
+
 def test_dummy():
     while True:
         print(gen_dummy_features())
+        time.sleep(1)
+
+
+def test_write():
+    while True:
+        append_dummy_row_to_csv('test.csv')
         time.sleep(1)
